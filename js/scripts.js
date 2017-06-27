da079743b31d2464a2c14d92c81f986982fae80b
José Pablo Iglesias <JPIglesias-Triangle@users.noreply.github.com>
2017-06-27 07:11:29 -0500

    Added Smooth-Scroll.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,21 @@
 function horn(){
        var audio = document.getElementById("horn-honk");
        audio.play();
-                 };
+};
+
+
+// Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
