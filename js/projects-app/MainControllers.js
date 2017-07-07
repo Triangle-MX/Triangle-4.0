91ba4e9025f089c4879b0bbccbf4146f7d327dd6
José Pablo Iglesias <JPIglesias-Triangle@users.noreply.github.com>
2017-07-07 12:30:02 -0500

    Public v1.6

--- js/projects-app/MainControllers.js
+++ js/projects-app/MainControllers.js
@@ -1,37 +1,43 @@
-app.controller('MainController', ['$scope', function($scope) { 
-  $scope.dprojects = 
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.dprojects =
    [
-   { 
-    name: 'Astros', 
-    type: 'Página Web',
+   {
+    name: 'Astros',
+    type: 'Desarrollo Web',
     thumb: '../img/Proyectos/Astros-Project.svg',
     link: 'http://astros.triangle.mx/'
-   }, 
-   { 
-    name: 'Learning English', 
+   },
+   {
+    name: 'Learning English',
     type: 'Colaboración',
     thumb: '../img/Proyectos/Learning-English-Project.svg',
     link: 'http://learning-english.triangle.mx/'
-   }, 
-   { 
-    name: 'BusinessSenior', 
-    type: 'Página Web',
+   },
+   {
+    name: 'BusinessSenior',
+    type: 'Desarrollo Web',
     thumb: '../img/Proyectos/BusinessSenior-Project.svg',
     link: 'http://businesssenior.com.mx/'
    },
+   {
+    name: 'Insight',
+    type: 'Desarrollo Web',
+    thumb: '../img/Proyectos/Insight-Project.svg',
+    link: 'https://triangle.mx/insight'
+   },
+   ];
+  $scope.nprojects = [
+   //{
+    //name: 'Kibo',
+    //type: 'Diseño Gráfico',
+    //thumb: '../img/Proyectos/Kibo-Project.svg',
+    //link: 'http://astros.triangle.mx/'
+   //}
    {
     name: 'Kida',
     type: 'Diseño Gráfico',
     thumb: '../img/Proyectos/Kida-Project.svg',
     link: 'http://kida.com.mx'
    }
-   ];
-  $scope.nprojects = [
-   //{ 
-    //name: 'Kibo', 
-    //type: 'Diseño Gráfico', 
-    //thumb: '../img/Proyectos/Kibo-Project.svg',
-    //link: 'http://astros.triangle.mx/'
-   //}       
    ];
 }]);
